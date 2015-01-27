3b549df3b69c7f01a4e7d9ddee3e23606c63bc86
Steve Nolen <technolengy@gmail.com>
2015-01-27 15:11:11 -0800

    actually look up this button

--- app.js
+++ app.js
@@ -75,7 +75,7 @@ oh.user.whoami().done(function(username){
       $('#modal-description').text(editing_doc[0]['description']);
       $('#modal-creator').val(editing_doc[0]['creator']);
       $('#modal-privacy').val(editing_doc[0]['privacy_state']);
-      $('#button-row').find('#document_id').val(editing_doc[0]['uuid']);
+      $("input[name='document_id']", "#button-row").val(editing_doc[0]['uuid']);
       var class_values = $.map(editing_doc[0]['class_role'], function(val,key){ return key;});
       $('#modal-class').multiselect('select', class_values);
       var campaign_values = $.map(editing_doc[0]['campaign_role'], function(val,key){ return key;});
