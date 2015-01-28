a1f25c9f7857af40a4ec16f78ea1a896668e10ec
Steve Nolen <technolengy@gmail.com>
2015-01-28 13:24:38 -0800

    maybe sort this way

--- app.js
+++ app.js
@@ -87,10 +87,10 @@ oh.user.whoami().done(function(username){
       $('#modal-delete').data('uuid', editing_doc[0]['uuid']);
       $("input[name='document_id']", "#detail-modal").val(editing_doc[0]['uuid']);
       class_values = $.map(editing_doc[0]['class_role'], function(val,key){ return key;});
-      class_values = _.sortBy(class_values, function(num) {return num;});
+      class_values = class_values.sort();
       $('#modal-class').multiselect('select', class_values);
       campaign_values = $.map(editing_doc[0]['campaign_role'], function(val,key){ return key;});
-      campaign_values = _.sortBy(campaign_values, function(num) { return num;});
+      campaign_values = campaign_values.sort();
       $('#modal-campaign').multiselect('select', campaign_values);
       if(editing_doc[0]['user_max_role'] == "reader") {
         $('.writer').prop('disabled', true);
