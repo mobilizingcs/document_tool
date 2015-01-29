a4d4dbdb8122113a6cfa270fd0937b543a9fd122
Steve Nolen <technolengy@gmail.com>
2015-01-29 07:13:18 -0800

    allow name to be changed, fix bytes sorting

--- assets/js/app.js
+++ assets/js/app.js
@@ -203,7 +203,7 @@ oh.user.whoami().done(function(username){
   function bytesToSize(bytes) {
    if(bytes == 0) return '0 Byte';
    var k = 1000;
-   var sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'];
+   var sizes = ['BB', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'];
    var i = Math.floor(Math.log(bytes) / Math.log(k));
    return (bytes / Math.pow(k, i)).toPrecision(3) + sizes[i];
 }
