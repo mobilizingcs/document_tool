654cf6ea2f30220cde6b965ed8a9936446f8348d
Steve Nolen <technolengy@gmail.com>
2015-10-28 06:50:35 -0700

    pull filter to left

--- assets/js/app.js
+++ assets/js/app.js
@@ -74,6 +74,7 @@ oh.user.whoami().done(function(username){
     });
    var table = $('#documents').DataTable( {
     "data": document_data,
+    "dom" : '<"pull-right"l><"pull-left"f>tip',
 	  "lengthMenu": [[25, 50, 100, -1], [25, 50, 100, "All"]],
 	  "oSearch": {"sSearch": "",
   	 "bRegex": true
